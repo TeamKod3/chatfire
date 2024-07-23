35919ae64430a3c9b1e6b6d8ceebf0366f89fd48
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-07-23 18:35:33 -0300

    a

--- src/api/class/instance.js
+++ src/api/class/instance.js
@@ -577,7 +577,7 @@ class WhatsAppInstance {
         })
 
         sock?.ev.on('messages.update', async (messages) => {
-            // console.log('messages.update')
+            console.log('messages.update')
             // console.dir(messages);
         })
         // sock?.ws.on('CB:call', async (data) => {
