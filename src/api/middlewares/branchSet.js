5133f315254cd6f7d46d78c9bb45865218cf0a18
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-07-30 18:21:08 -0300

    updated log

--- src/api/middlewares/branchSet.js
+++ src/api/middlewares/branchSet.js
@@ -4,8 +4,10 @@ function branchSet(req, res, next) {
     const branch = req.get('branch') || req.headers['branch'];
     console.log({branch})
     if(!branch) {
+        console.log('entrou no if de não existir branch')
         global.production = true
     } else {
+        console.log('branch existe')
         global.production = branch === 'prod'
     }
     next()
