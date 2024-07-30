f9065eff55f28f5c36d3d742cdb2175f3be841ce
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-07-30 18:24:27 -0300

    updated log

--- src/api/helper/sendSupabase.js
+++ src/api/helper/sendSupabase.js
@@ -3,8 +3,8 @@ const { unlinkSync, readFileSync } = require('fs');
 
 // Create a single supabase client for interacting with your database
 
-const uriSupabase = global.production ? 'https://fntyzzstyetnbvrpqfre.supabase.co' : 'https://ndxedvhrarwaiyrcvdzp.supabase.co'
-const apiKeySupabase = global.production ? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZudHl6enN0eWV0bmJ2cnBxZnJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTExMTM0NzksImV4cCI6MjAwNjY4OTQ3OX0.eaod7DsHG3Pc1ZBFSmvr3r6by-MtNf0hzjgjXzdN3Jk' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5keGVkdmhyYXJ3YWl5cmN2ZHpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4Njk5OTEsImV4cCI6MjAzNzQ0NTk5MX0.lqwaEjPF5VuVWgxjZYdU_QWQELDknA9uYUzt-aoSRU4'
+const uriSupabase = global.production === true ? 'https://fntyzzstyetnbvrpqfre.supabase.co' : 'https://ndxedvhrarwaiyrcvdzp.supabase.co'
+const apiKeySupabase = global.production === true ? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZudHl6enN0eWV0bmJ2cnBxZnJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTExMTM0NzksImV4cCI6MjAwNjY4OTQ3OX0.eaod7DsHG3Pc1ZBFSmvr3r6by-MtNf0hzjgjXzdN3Jk' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5keGVkdmhyYXJ3YWl5cmN2ZHpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4Njk5OTEsImV4cCI6MjAzNzQ0NTk5MX0.lqwaEjPF5VuVWgxjZYdU_QWQELDknA9uYUzt-aoSRU4'
 
 
  /** * Insert a new record into the specified table * @param {string} tableName 
--- src/api/middlewares/branchSet.js
+++ src/api/middlewares/branchSet.js
@@ -2,12 +2,9 @@ global.production = true
 
 function branchSet(req, res, next) {
     const branch = req.get('branch') || req.headers['branch'];
-    console.log({branch})
     if(!branch) {
-        console.log('entrou no if de não existir branch')
         global.production = true
     } else {
-        console.log('branch existe')
         global.production = branch === 'prod'
     }
     next()
