73ec24311f93f6450dd8d72af9717f4f00be016c
Carlos Cesar Baratella Junior <molusko.rp@hotmail.com>
2024-10-31 10:00:38 -0300

    Update version baileys

--- src/api/class/instance.js
+++ src/api/class/instance.js
@@ -656,14 +656,18 @@ class WhatsAppInstance {
         const conexao = await getConexao(phone, this.empresaId, this.clientId)
         if(conexao) {
             if(conexao["Status"] === true){
-                await updateDataInTable('conexoes', {id: this.clientId}, {Nome: name, 'Número': phone, status_conexao: 'Duplicado', qrcode: ''})
+                if(conexao['Nome']) {
+                    await updateDataInTable('conexoes', {id: this.clientId}, {'Número': phone, status_conexao: 'Duplicado', qrcode: ''})
+                } else {
+                    await updateDataInTable('conexoes', {id: this.clientId}, {Nome: name, 'Número': phone, status_conexao: 'Duplicado', qrcode: ''})
+                }
+                
                 this.duplicado = true
                 await this.instance.sock?.logout()
                 return
             } else {
-                await updateDataInTable('conexoes', {id: this.clientId}, {Nome: name, 'Número': phone, status_conexao: 'pronto', qrcode: '', Status: true, instance_key: this.key})
+                await updateDataInTable('conexoes', {id: this.clientId}, {'Número': phone, status_conexao: 'pronto', qrcode: '', Status: true, instance_key: this.key})
                 await deleteDataFromtable('conexoes', {id: conexao.id})
-                //await updateDataInTable('conexoes', {id: conexao.id}, {status_conexao: 'Duplicado', qrcode: '', Status: false}) 
                 const setores = await fetchSetores(this.empresaId)
                 for(const setor of setores) {
                     await sendDataToSupabase('setor_conexao', {
